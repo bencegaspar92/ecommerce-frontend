8f9c67c61aae7de1ca267ac96c4cf07efeaef271
Gáspár Bence <bence.gaspar@sightspot.hu>
2020-12-18 16:41:25 +0100

    implemented sidebar's beta. (animation and few things missing)

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit {
+export class NavComponent {
+  isMenuOpen = false;
+  contentMargin = 240;
 
-  constructor() { }
-
-  ngOnInit() {
+  onToolbarMenuToggle() {
+    this.isMenuOpen = !this.isMenuOpen;
+    !this.isMenuOpen ? this.contentMargin = 70 : this.contentMargin = 240;
   }
-
 }
