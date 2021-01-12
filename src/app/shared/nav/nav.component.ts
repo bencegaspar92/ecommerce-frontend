c5575c66b3952517a29e3b5516f6dcfb2c1334bc
Gáspár Bence <bence.gaspar@sightspot.hu>
2021-01-12 17:12:05 +0100

    redesigned page and implemented a new navbar.

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -1,18 +1,9 @@
 import { Component } from '@angular/core';
-import {Animations} from '../animations/animations';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss'],
-  animations: [Animations.slideIn]
 })
 export class NavComponent {
-  isMenuOpen = false;
-  contentMargin = 240;
-
-  onToolbarMenuToggle() {
-    this.isMenuOpen = !this.isMenuOpen;
-    !this.isMenuOpen ? this.contentMargin = 70 : this.contentMargin = 240;
-  }
 }
